3eed6796c8a619341dafec526662390260ea0c57
June <June@qq.com>
2022-05-06 19:47:16 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("Hello world!");
+        System.out.println("hello hot-fix");
     }
 }
